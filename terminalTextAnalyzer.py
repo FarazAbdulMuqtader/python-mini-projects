91a11be0ad24dd1f6825979c096a6bafd87a193f
Faraz Abdul Muqtader <abdulmuqtaderf@gmail.com>
2026-08-20 19:23:00 +0500

    Refactor terminalTextAnalyzer to clean text input
    Refactor text processing into a clean_text function and remove special characters.

--- terminalTextAnalyzer.py
+++ terminalTextAnalyzer.py
@@ -1,8 +1,22 @@
-txt=str(input("Write multi-line statements: "))
+import re
 
-convert_lowercase=txt.lower()
+def clean_text(message):
+    convert_lowercase=message.lower()
+    print("Lowercase: ", convert_lowercase)
+    special_chars=r"[@,./&^%$#]"
+    txt=re.sub(special_chars, "", convert_lowercase)
+    print("Special characters removed: ", txt)
+    words=txt.split()
+    print("Words: ", words)
+    word_count=len(words)
+    print("Word count: ", word_count)
+    word_data=[]
+    for i in range(len(words)):
+        dict={"word":words[i], "count":words.count(words[i])}
+        if dict not in word_data:
+            word_data.append(dict)
+    print("Word data: ", word_data)
 
-special_chars = "!@#$%^&*()_+-=[]{}|;:'\",.<>?/"
-if(punc:=convert_lowercase.replace(special_chars, "")):
-    print(punc)
 
+message=str(input("Write multi-line statements: "))
+clean_text(message)
